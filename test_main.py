3e8810e9c5a529b2e94b26adcf7331830bc02755
nmcanenydroz <142832766+nmcanenydroz@users.noreply.github.com>
2023-08-22 03:17:03 +0000

    nmcanenydroz assignment 1

--- test_main.py
+++ test_main.py
@@ -1,7 +1,8 @@
 from main import *
 
+
 def test_myfunction():
-	""" done. """
-	assert myfunction('Hello') == 'Hello'
-	assert myfunction(1) == 1
-	assert myfunction(6) == -1
+  """ done. """
+  assert myfunction('Hello') == 'Hello'
+  assert myfunction(1) == 1
+  assert myfunction(6) == 6
